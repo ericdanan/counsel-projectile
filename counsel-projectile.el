fc993c9275a0bb755e16f8c74a62ce7b877a1d09
Eric Danan <admin@ericdanan.net>
2016-04-13 11:02:06 +0200

    change counsel version requirement

--- counsel-projectile.el
+++ counsel-projectile.el
@@ -7,7 +7,7 @@
 ;; Created: 2016-04-11
 ;; Keywords: project, convenience
 ;; Version: 0.1
-;; Package-Requires: ((counsel "20160411") (projectile "0.13.0"))
+;; Package-Requires: ((counsel "0.7.0") (projectile "0.13.0"))
 
 ;; This file is NOT part of GNU Emacs.
 
