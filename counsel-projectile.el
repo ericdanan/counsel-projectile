852011d217512bd2a039c1edabb0355fb7a0b3d7
Eric Danan <admin@ericdanan.net>
2016-09-15 13:54:54 +0200

    update commentary

--- counsel-projectile.el
+++ counsel-projectile.el
@@ -30,9 +30,11 @@
 
 ;;; Commentary:
 ;;
-;; This library tries to do something similar to `helm-projectile',
-;; but using `ivy' instead of `helm'. The main function is
-;; `counsel-projectile'.
+;; Projectile has native support for using ivy as its completion
+;; system. Counsel-projectile provides further ivy integration into
+;; projectile by taking advantage of ivy's mechanism to select from a
+;; list of actions and/or apply an action without leaving the comlpetion
+;; session. It is inspired by helm-projectile. See the README for more details.
 ;;
 ;;; Code:
 
